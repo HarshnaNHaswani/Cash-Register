fe77fd43e015e297568da69d9b345c3eebaa0707
Harshna <hharshna.n@gmail.com>
2021-10-20 18:32:19 +0530

    Revert "fix"
    This reverts commit 4766d94d6211dd154ebdbda709490cea1cf6abda.

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const billAmount = document.querySelector("#bill-amount");
-const cashGiven = document.getElementById("cash-given");
+const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
-const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 1];
-// if(billAmount.value === "") {cashGiven.disabled = true;}
+const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 1]
 checkButton.addEventListener("click", validateBillandCashAmount);
 
 function validateBillandCashAmount(){
